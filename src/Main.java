6b331ae0332c0d91447da27dbe4d08c8af9e2baf
AlexandrFateev <mailboxfav@gmail.com>
2023-05-29 19:12:58 +0500

    Complete task 2

--- src/Main.java
+++ src/Main.java
@@ -1,58 +1,111 @@
-import java.io.File;
-import java.io.FileWriter;
 import java.io.IOException;
+import java.io.ObjectOutputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Arrays;
+import java.util.List;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipOutputStream;
 
 public class Main {
 
-    public static File gameDirectory = new File("D://Games");
+    public static String gameDirectory = "D://GamesNET";
 
-    public static String[] dirs = {"src", "res", "savegames", "temp"};
-    public static String[] srcDir = {"main", "test"};
-    public static String[] mainFiles = {"Main.java", "Utils.java"};
-    public static String[] resDir = {"drawables", "vectors", "icons"};
-    public static File tempFile = new File(new File(gameDirectory, "temp"), "temp.txt");
+    public static String[] dirs = {
+            "src", "res", "savegames", "temp",
+            "src//main", "src//test",
+            "res//drawables", "res//vectors", "res//icons"};
+    public static String[] files = {"src//main//Main.java", "src//main//Utils.java"};
+    public static String tempFile = "temp//temp.txt";
     public static StringBuilder logMessage = new StringBuilder();
 
     public static void main(String[] args) {
-        createDirs(gameDirectory, dirs);
-        createDirs(new File(gameDirectory, "src"), srcDir);
-        createFiles(new File(new File(gameDirectory, "src"), "main"), mainFiles);
-        createDirs(new File(gameDirectory, "res"), resDir);
-        System.out.println(logMessage.toString());
+        prepareToInstall();
+
+        GameProgress game1 = new GameProgress(100, 15, 23, 60);
+        GameProgress game2 = new GameProgress(120, 25, 26, 45);
+        GameProgress game3 = new GameProgress(80, 10, 18, 90);
+        saveGame("save01", game1);
+        saveGame("save02", game2);
+        saveGame("save03", game3);
+        zipFiles("zip.zip", Arrays.asList("save01", "save02", "save03"));
+    }
+
+    private static void zipFiles(String fileName, List<String> listFiles) {
+        try (ZipOutputStream zout = new ZipOutputStream(
+                Files.newOutputStream(Paths.get(gameDirectory, "savegames", fileName)))) {
+            for (String file : listFiles) {
+                Path path = Paths.get(gameDirectory, "savegames", file);
+                zout.putNextEntry(new ZipEntry(path.getFileName().toString()));
+                Files.copy(path, zout);
+                zout.closeEntry();
+                Files.delete(path);
+            }
+
+        } catch (Exception ex) {
+            System.out.println(ex.getMessage());
+        }
+    }
+
+    private static void saveGame(String fileName, GameProgress game) {
+        try (ObjectOutputStream oos =
+                     new ObjectOutputStream(
+                             Files.newOutputStream(Paths.get(gameDirectory, "savegames", fileName)))) {
+            oos.writeObject(game);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private static void prepareToInstall() {
+        createMainDir();
+        createSubDirs();
+        createFiles();
         writeToLog(logMessage.toString());
     }
 
-    static void createDirs(File dir, String[] dirs) {
-        for (String d : dirs) {
-            if (new File(dir, d).mkdir()) {
-                logMessage.append("Create folder: " + d + "\n");
-            }
-        }
-    }
-
-    static void createFiles(File dir, String[] files) {
+    private static void createFiles() {
         for (String file : files) {
             try {
-                if (new File(dir, file).createNewFile()) {
-                    logMessage.append("Create file: " + file + "\n");
-                }
+                Path path = Paths.get(gameDirectory, file);
+                Files.write(path, "".getBytes());
+                logMessage.append("Создан файл: " + path.toAbsolutePath() + "\n");
             } catch (IOException e) {
-                System.out.println(e.getMessage());
+                logMessage.append("Ошибка создания файла: " + file + "\n");
             }
         }
     }
 
-    static void writeToLog(String message) {
-        try {
-            tempFile.createNewFile();
-        } catch (IOException e) {
-            System.out.println(e.getMessage());
+    private static void createSubDirs() {
+        for (String dir : dirs) {
+            try {
+                Path path = Paths.get(gameDirectory, dir);
+                Files.createDirectories(path);
+                logMessage.append("Создана папка: " + path.toAbsolutePath() + "\n");
+            } catch (IOException e) {
+                logMessage.append("Ошибка создания папки: " + dir + "\n");
+            }
         }
-        try (FileWriter fw = new FileWriter(tempFile, false)) {
-            fw.write(message);
-            fw.flush();
+    }
+
+    private static void createMainDir() {
+        try {
+            Path path = Paths.get(gameDirectory);
+            Files.createDirectories(path);
+            logMessage.append("Создана папка: " + path.toAbsolutePath() + "\n");
         } catch (IOException e) {
-            System.out.println(e.getMessage());
+            logMessage.append("Ошибка создания папки: " + gameDirectory + "\n");
+        }
+    }
+
+
+    static void writeToLog(String message) {
+        Path path = Paths.get(gameDirectory, tempFile);
+        try {
+            Files.write(path, message.getBytes());
+        } catch (IOException e) {
+            throw new RuntimeException(e);
         }
     }
 }
